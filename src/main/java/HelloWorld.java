2857b06a836f6dd2c29c17f2b1518569751e51b5
zff85277708 <602192932@qq.com>
2022-12-10 20:03:23 +0800

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,24 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorld extends HttpServlet {
+    private String message;
+
+    @Override
+    public void init() {
+        message = "HelloWorld";
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 设置响应内容类型
+        response.setContentType("text/html");
+
+        // 实际的逻辑是在这里
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + "</h1>");
+    }
+}
